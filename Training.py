72bdb1cbb05c3d34b7e668ca16312eacdf9afec8
unknown <andpavpar@gmail.com>
2019-10-12 00:52:38 +0200

    Creada classe Training

---
+++ Training.py
@@ -0,0 +1,9 @@
+class Training:
+    def __init__(self, accidents, vehicles, target):
+        self.accidents = accidents
+        self.vehicles = vehicles
+        self.target = target
+        
+
+
+
