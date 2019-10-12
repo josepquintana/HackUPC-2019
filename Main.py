b2b977761b7771eb5110c9e098615eb47e2ebf49
josep.quintana <josep.quintana.44@gmail.com>
2019-10-12 18:59:11 +0200

    some shit

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import LoadData as ld
 import Training as tr
 from sklearn.metrics import f1_score
 
+
 def main():
     print("Hello World!")
     accidents_data = ld.AccidentsData()
@@ -15,12 +16,22 @@ def main():
     y_train = merged_data.get_target_train()
 
     training = tr.Training(X_train, y_train)
-    training.knnTraining()
 
+#   training.knnTraining()
+#    y_pred = training.model.predict(X_test)
+#    print("KNN: %.10f" % f1_score(y_test, y_pred))
+
+    training.decisionTreeTraining()
     y_pred = training.model.predict(X_test)
+    print("CLF: %.10f" % f1_score(y_test, y_pred))
 
+#    training.supportVectorMachinesTraining()
+#    y_pred = training.model.predict(X_test)
+#    print("SVM: %.10f" % f1_score(y_test, y_pred))
 
-    print(f1_score(y_test,y_pred))
+#    training.logisticRegressionTraining()
+#    y_pred = training.model.predict(X_test)
+#    print("LRT: %.10f" % f1_score(y_test, y_pred))
 
     #target.to_csv(r'output/target.csv')
     #print(merged.loc[merged['accident_id'] == 583864]['number_of_vehicles'])
--- Training.py
+++ Training.py
@@ -1,7 +1,11 @@
+from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import tree
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import roc_auc_score
+import numpy as np
 
 
 class Training:
@@ -17,9 +21,28 @@ class Training:
         # Cridar la funcion de fit amb els valors desitjats, buscar l'hiper paràmetre adequat
 
     def decisionTreeTraining(self):
-        clf = tree.DecisionTreeClassifier()
-        clf.fit(self.training, self.target)
-        self.model = clf
+        params = {
+                #'criterion': ('gini', 'entropy'),
+                #'splitter': ('best', 'random'),
+                #'max_depth': ('None', [250, 1000]),
+                #'min_samples_split': [2, 3, 4, 5, 6, 7, 8, 9, 10],
+                #'min_samples_leaf': [2, 3, 4, 5, 6, 7, 8, 9, 10],
+                #'min_weight_fraction_leaf': [0, 0.5],
+                #'max_features': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                #'min_impurity_decrease': [0, 1, 2, 3, 4, 5],
+                #'presort': (True, False)
+            }
+
+        params = {'max_depth': [750, 1250]}
+
+        dtc = GridSearchCV(DecisionTreeClassifier(), params, n_jobs=-1, verbose=1)
+        dtc.fit(self.training, self.target)
+        print(dtc.best_estimator_)
+        self.model = dtc
+
+        #clf = tree.DecisionTreeClassifier()
+        #clf.fit(self.training, self.target)
+        #self.model = clf
         # Cridar la funcion de fit amb els valors desitjats
 
     def supportVectorMachinesTraining(self):
