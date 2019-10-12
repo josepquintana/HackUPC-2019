364a9f3191439fb7a8732a80014837441e8bea29
unknown <andpavpar@gmail.com>
2019-10-12 02:11:29 +0200

    Arreglats errors compilacio

--- Training.py
+++ Training.py
@@ -1,7 +1,7 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import tree
 from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression as lr
+from sklearn.linear_model import LogisticRegression
 
 
 
@@ -13,19 +13,19 @@ class Training:
         self.vehicles = vehicles
         self.target = target
 
-    def knnTraining():
+    def knnTraining(self):
         knn = KNeighborsClassifier(n_neighbors=15)
-        ### Cridar la funcion de fit amb els valors desitjats, buscar l'hiper paràmetre adequat ###
+        # Cridar la funcion de fit amb els valors desitjats, buscar l'hiper paràmetre adequat
 
     def decisionTreeTraining(self):
         clf = tree.DecisionTreeClassifier()
-        ### Cridar la funcion de fit amb els valors desitjats ###
+        # Cridar la funcion de fit amb els valors desitjats
 
 
     def supportVectorMachinesTraining(self):
-        svm = SVC.SVC()
-        ### Cridar la funcion de fit amb els valors desitjats ###
+        svm = SVC()
+        # Cridar la funcion de fit amb els valors desitjats
 
     def logisticRegressionTraining(self):
-        lrt = lr.LogisticRegression(C=1, penalty='l2', tol=0.00001)
-        ### Tocar paràmetres i cridar la funció fit ###
+        lrt = LogisticRegression(C=1, penalty='l2', tol=0.00001)
+        # Tocar paràmetres i cridar la funció fit
