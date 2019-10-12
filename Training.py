cc715698c69a8caf37b663aaa30b1df26029c21a
unknown <andpavpar@gmail.com>
2019-10-12 10:00:18 +0200

    Tractem accidents+vehicles com 1 taula

--- Training.py
+++ Training.py
@@ -8,24 +8,30 @@ from sklearn.linear_model import LogisticRegression
 
 
 class Training:
-    def __init__(self, accidents, vehicles, target):
-        self.accidents = accidents
-        self.vehicles = vehicles
+
+    def __init__(self, training, target):
+        self.training = training
         self.target = target
 
     def knnTraining(self):
         knn = KNeighborsClassifier(n_neighbors=15)
+        knn.fit(self.training, self.target)
         # Cridar la funcion de fit amb els valors desitjats, buscar l'hiper paràmetre adequat
 
     def decisionTreeTraining(self):
         clf = tree.DecisionTreeClassifier()
+        clf.fit(self.training, self.target)
         # Cridar la funcion de fit amb els valors desitjats
 
 
     def supportVectorMachinesTraining(self):
         svm = SVC()
+        svm.fit(self.training, self.target)
+
         # Cridar la funcion de fit amb els valors desitjats
 
     def logisticRegressionTraining(self):
         lrt = LogisticRegression(C=1, penalty='l2', tol=0.00001)
+        lrt.fit(self.training, self.target)
+
         # Tocar paràmetres i cridar la funció fit
