9a8c48c634978b1025e3995623d0afb70820ec53
Andreu Punsola <andreu4ps@gmail.com>
2019-10-12 15:17:45 +0200

    ja tenim fscoreeeees

--- Main.py
+++ Main.py
@@ -1,22 +1,26 @@
 import pandas as pd
 import LoadData as ld
 import Training as tr
+from sklearn.metrics import f1_score
 
 def main():
     print("Hello World!")
     accidents_data = ld.AccidentsData()
     vehicles_data = ld.VehiclesData()
-    merged_data = ld.MergedData(accidents_data, vehicles_data)
+    merged_data = ld.MergedData(accidents_data, vehicles_data, 0.8)
 
-    merged = merged_data.get_merged()
-    target = merged_data.get_target()
+    X_test = merged_data.get_merged_test()
+    y_test = merged_data.get_target_test()
+    X_train = merged_data.get_merged_train()
+    y_train = merged_data.get_target_train()
 
-    #merged.to_csv('out/merged.csv', sep=',')
-    #target.to_csv('out/target.csv', sep=',')
+    training = tr.Training(X_train, y_train)
+    training.knnTraining()
 
-    training = tr.Training(merged, target)
-    training.decisionTreeTraining()
-    print(training.model)
+    y_pred = training.model.predict(X_test)
+
+
+    print(f1_score(y_test,y_pred))
 
     #target.to_csv(r'output/target.csv')
     #print(merged.loc[merged['accident_id'] == 583864]['number_of_vehicles'])
